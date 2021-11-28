3f3d7d5877133dc56b2c2ae9d502f8bdfb6ffbc8
CB38 <cole3@outlook.com>
2021-11-28 22:41:43 +0000

    added ability to view site without logging in

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[ index show ] 
 
   # GET /projects or /projects.json
   def index
@@ -13,7 +13,9 @@ class ProjectsController < ApplicationController
 
   # GET /projects/new
   def new
-        
+    
+    
+
     @project = Project.new
   end
 
